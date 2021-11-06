59b5a1ce69bb44d25b5edf8869c579871f9f4ee1
Geiner <davidlararada1062@hotmail.com>
2021-11-06 12:36:38 -0500

    se instalo module cross-env

--- connection.js
+++ connection.js
@@ -1,15 +1,14 @@
-const  mongoose = require("mongoose")
-const  database = require("./database")
+const mongoose = require("mongoose");
+const database = require("./database");
 
 const options = {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
-}
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
-let connection = mongoose.connect(database.url, options)
-.then(
-  () => console.log("Database connected"),
-  error => console.error("Database could't be connected to: " + error)
-)
+let connection = mongoose.connect(database.url, options).then(
+  () => console.log("Database connected funciono "),
+  (error) => console.error("Database could't be connected to: " + error)
+);
 
-module.exports = connection
+module.exports = connection;
